8045c1f1833a80e2c3ae4d278e95eb42785a47a1
Edouard CATTEZ <edouard.cattez@sfr.fr>
2015-10-14 09:19:21 +0200

    TestCase

--- test/fr/lordrski/maze/CoordinatesTestCase.java
+++ test/fr/lordrski/maze/CoordinatesTestCase.java
@@ -17,9 +17,12 @@ package fr.lordrski.maze;
  * 
  * @author Edouard CATTEZ <edouard.cattez@sfr.fr> (La 7 Production)
  */
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
+
+import fr.lordrski.maze.entity.Coordinates;
 
 public class CoordinatesTestCase {
 
